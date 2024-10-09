ae580fd150f23a91a46dbad82bc0783af06edbae
Sol <gerlorework@gmail.com>
2024-10-09 12:45:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const nombre = document.getElementById('nombre');
+const edad = document.getElementById('edad');
+const pais = document.getElementById('pais');
+const enviar = document.getElementById('enviar')
+
+enviar.addEventListener('submit', () =>{
+    
+})
